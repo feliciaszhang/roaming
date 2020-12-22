f1ba482aef1398f5450967474c4be671b168c704
feliciaszhang <feliciaszhang@gmail.com>
2020-12-22 14:41:07 -0800

    add message field

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,24 +7,50 @@ import {
   Code,
   Grid,
   theme,
+  Button,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import SocketService from './SocketService';
+import SocketService from "./SocketService";
+import { Form, Formik } from "formik";
+import { InputField } from "./components/InputField";
 
-function App() {
-  const ChatContext: React.Context<SocketService> = createContext(new SocketService());
+export const App = () => {
+  const ChatContext: React.Context<SocketService> = createContext(
+    new SocketService()
+  );
   const socket: SocketService = useContext(ChatContext);
-  socket.connect()
-  
+  socket.connect();
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Text>
-              Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-            </Text>
+            <Formik
+              initialValues={{ message: "" }}
+              onSubmit={(values) => socket.message(values.message)}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <InputField
+                    name="message"
+                    placeholder="message"
+                    label="Message"
+                  />
+                  <Button
+                    mt={4}
+                    type="submit"
+                    colorScheme="teal"
+                    textColor="white"
+                    variant="solid"
+                    isLoading={isSubmitting}
+                  >
+                    Send
+                  </Button>
+                </Form>
+              )}
+            </Formik>
           </VStack>
         </Grid>
       </Box>
