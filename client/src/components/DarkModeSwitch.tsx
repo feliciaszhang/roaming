53886524590334af8f4c839eed44a8875d519c5b
feliciaszhang <feliciaszhang@gmail.com>
2020-12-22 22:59:52 -0800

    implement color switch when toggled

--- client/src/components/DarkModeSwitch.tsx
+++ client/src/components/DarkModeSwitch.tsx
@@ -1,8 +1,18 @@
 import { useColorMode, Switch } from '@chakra-ui/react'
+import React, { InputHTMLAttributes, useEffect, useRef } from "react";
 
-export const DarkModeSwitch = () => {
+type DarkModeSwitchProps = InputHTMLAttributes<HTMLInputElement> & {
+  handleToggleColor: (isDark:boolean) => void;
+};
+
+export const DarkModeSwitch: React.FC<DarkModeSwitchProps> = ({handleToggleColor}) => {
   const { colorMode, toggleColorMode } = useColorMode()
   const isDark = colorMode === 'dark'
+
+  useEffect(() => {
+    handleToggleColor(isDark)
+  })
+
   return (
     <Switch
       position="fixed"
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useRef } from "react";
 import {
   ChakraProvider,
   Box,
@@ -14,21 +14,34 @@ import { MessageList } from "../components/MessageList";
 
 export const App = () => {
   const { messageList, sendMessage } = useSocket("12");
+  const footer = useRef<HTMLInputElement>(null);
+  let isDark: boolean = true;
 
   const handleSendMessage = (values: { message: string }) => {
     sendMessage(values.message);
+  };
+
+  const handleToggleColor = (_isDark: boolean) => {
+    isDark = _isDark;
+    footer.current.style.background = isDark? theme.colors.gray[800] : "white"
   };
 
   return (
     <ChakraProvider theme={theme}>
       <Box fontSize="xl">
         <Grid minH="100vh" p={3} paddingBottom="90px">
-          <DarkModeSwitch />
+          <DarkModeSwitch handleToggleColor={handleToggleColor} />
           <VStack spacing={8}>
             <Flex width="100%" alignItems="flex-start" overflow="hidden">
               <MessageList messageList={messageList} />
             </Flex>
-            <Flex bottom={0} position="fixed" display="block" width="100%" bg={"gray.800"}>
+            <Flex
+              ref={footer}
+              bottom={0}
+              position="fixed"
+              display="block"
+              width="100%"
+            >
               <SendField handleSendMessage={handleSendMessage} />
             </Flex>
           </VStack>
