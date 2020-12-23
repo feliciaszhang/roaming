226b7aa065ceb397d5ec17d45201e287aedf2ec1
feliciaszhang <feliciaszhang@gmail.com>
2020-12-23 13:58:37 -0800

    add join page

--- client/src/pages/join.tsx
+++ client/src/pages/join.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { InputHTMLAttributes } from "react";
 import { InputField } from "../components/InputField";
-import { Box, Button, Grid } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { Formik, Form } from "formik";
 
 const Join: React.FC<{}> = ({}) => {
-  return <Button>here</Button>;
+  return (
+    <Box mx="auto">
+      <Formik
+        initialValues={{ room: "" }}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setSubmitting(false);
+          resetForm();
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+              <Flex align="center" direction="column">
+            <InputField name="room" placeholder="room" label="Room" />
+            <Button
+              ml={2}
+              type="submit"
+              colorScheme="teal"
+              variant="solid"
+              isLoading={isSubmitting}
+            >
+              Join
+            </Button>
+            </Flex>
+          </Form>
+        )}
+      </Formik>
+    </Box>
+  );
 };
 
 export default Join;
