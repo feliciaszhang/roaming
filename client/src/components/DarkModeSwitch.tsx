84188b07bdfe942f765db63ea9eec77f50413784
feliciaszhang <feliciaszhang@gmail.com>
2020-12-23 19:27:11 -0800

    improve ui

--- client/src/components/DarkModeSwitch.tsx
+++ client/src/components/DarkModeSwitch.tsx
@@ -1,13 +1,20 @@
-import { useColorMode, Switch, theme } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { useColorMode, Switch, theme, IconButton } from "@chakra-ui/react";
+import React from "react";
 
 export const DarkModeSwitch: React.FC<{}> = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
+  const bgColor: string = isDark
+    ? theme.colors.gray[700]
+    : theme.colors.gray[200];
 
   return (
-    <Switch
-      isChecked={isDark}
-      onChange={toggleColorMode}
+    <IconButton
+      aria-label="ToggleColorMode"
+      bg={bgColor}
+      onClick={toggleColorMode}
+      icon={isDark ? <SunIcon w={6} h={6} /> : <MoonIcon w={6} h={6} />}
     />
   );
 };
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -4,48 +4,75 @@ import {
   Flex,
   HStack,
   theme,
-  Container,
   useColorMode,
   Link,
+  Spacer,
+  IconButton,
+  Avatar,
 } from "@chakra-ui/react";
 import React from "react";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import NextLink from "next/link";
 import { signin, signout, useSession } from "next-auth/client";
+import { BellIcon, CalendarIcon, ChatIcon } from "@chakra-ui/icons";
 
 export const NavBar: React.FC<{}> = () => {
   const [session, loading] = useSession();
   const { colorMode, toggleColorMode } = useColorMode();
+  const bgColor: string =
+    colorMode === "dark" ? theme.colors.gray[700] : theme.colors.gray[200];
 
   return (
     <Flex
-      p={5}
-      background={
-        colorMode === "dark" ? theme.colors.gray[700] : theme.colors.gray[200]
-      }
+      p={4}
+      background={bgColor}
       top={0}
       position="fixed"
       display="block"
       width="100%"
     >
-      <Flex align="center" justifyContent="flex-end">
-        <HStack spacing={4}>
-          {!session && (
-            <NextLink href="/api/auth/signin">
-              <Link>Sign in</Link>
-            </NextLink>
-          )}
-          {session && (
-            <>
-              <Box>{session.user.email}</Box>
-              <NextLink href="/api/auth/signout">
-                <Link>Sign out</Link>
-              </NextLink>
-            </>
-          )}
-          <DarkModeSwitch />
+      <HStack>
+        <HStack spacing={10}>
+        <IconButton
+          aria-label="Calendar"
+          bg={bgColor}
+          icon={<CalendarIcon w={6} h={6} />}
+        />
+        <IconButton
+          aria-label="Messages"
+          bg={bgColor}
+          icon={<ChatIcon w={6} h={6} />}
+        />
+        <IconButton
+          aria-label="Notifications"
+          bg={bgColor}
+          icon={<BellIcon w={6} h={6} />}
+        />
         </HStack>
-      </Flex>
+        <Spacer />
+        <Flex align="center" justifyContent="flex-end">
+          <HStack spacing={4}>
+            {!session && (
+              <NextLink href="/api/auth/signin">
+                <Link>Sign in</Link>
+              </NextLink>
+            )}
+            {session && (
+              <>
+                <Avatar
+                  size="xs"
+                  name={session.user.name}
+                  src={session.user.image}
+                />
+                <NextLink href="/api/auth/signout">
+                  <Link>Sign out</Link>
+                </NextLink>
+              </>
+            )}
+            <DarkModeSwitch />
+          </HStack>
+        </Flex>
+      </HStack>
     </Flex>
   );
 };
